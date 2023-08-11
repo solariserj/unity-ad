a36c536fc54a008c0878b58e676b1a61fab8b019
Alexandru <alexandru@Serj-iMac.local>
2023-08-11 15:18:03 +0300

    feat(proj): init

---
+++ unityAds/unityAdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  unityAdsApp.swift
+//  unityAds
+//
+//  Created by Alexandru on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct unityAdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
