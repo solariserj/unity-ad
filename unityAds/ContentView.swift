2ce014cc4b391c1f975a96cc81bfab15525999c8
Alexandru <alexandru@Serj-iMac.local>
2023-08-11 15:58:02 +0300

    proj: prepare for ads

--- unityAds/ContentView.swift
+++ unityAds/ContentView.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    func showAds() {
+        print("Show Ad")
+    }
+    
+}
 
 struct ContentView: View {
+    
+    @ObservedObject var viewModel = ContentViewModel()
+    
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 50) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            
             Text("Hello, world!")
+            
+            Button("Show Ad") {
+                viewModel.showAds()
+            }
         }
         .padding()
     }
